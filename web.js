2338b653ce7b1a8a8edd68aecb17a4bea2ea824f
Gary Soedarsono <garysoed@gmail.com>
2014-06-11 19:19:55 -0700

    server

--- web.js
+++ web.js
@@ -9,6 +9,17 @@ app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
+var data = {};
+var maxId = 0;
+app.post('test', function(req, res) {
+  var clientId = null;
+  if (!req.get('clientId')) {
+    clientId = maxId++;
+  }
+  res.set('clientId', clientId);
+  res.set('data', req.get('data'));
+});
+
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
